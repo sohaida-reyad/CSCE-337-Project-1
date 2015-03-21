462deee6069fe9f8a153dfb1f41af5b74f9d7ca9
sohaida-reyad <mac@Macs.local>
2015-03-21 21:33:08 +0200

    updates 9:33

--- ui - updated /Logic-Circuit-Beautifier/mainwindow.cpp
+++ ui - updated /Logic-Circuit-Beautifier/mainwindow.cpp
@@ -6,6 +6,11 @@
 #include <QFileDialog>
 #include <QFileInfo>
 #include <QProcess>
+#include <QString>
+
+#include "DAG.h"
+#include "gate.h"
+#include "TopoSort.h"
 
 QString VerilogFileName;
 
@@ -42,9 +47,81 @@ void MainWindow::on_pushButton_clicked()
 
     // RUN
 
+    /*
     QString program = "/Users/mac/Library/Developer/Xcode/DerivedData/CSCE337Project1-groosmpjonvudfaudxeyovpzjfoy/Build/Products/Debug/CSCE337Project1";
-
     QProcess process;
     process.startDetached(program, QStringList() << fileName);
+    */
+
+    /**/
+
+    DAG netlist;
+    gate * g, * g1;
+
+    //string file = "//Users//mac//Desktop//folder//parsed_booth.g.v.txt";
+    //string file = argv[1];
+
+    unsigned int numberGates;
+
+    //if (netlist->openFile(argv[1]))
+    if (netlist.openFile(fileName))
+    {
+        netlist.readFile();
+        numberGates = netlist.getGatesCounter();
+
+        netlist.adjMatrix.resize(numberGates);
+        for (qint16 i = 0; i < numberGates; i++)
+            netlist.adjMatrix[i].resize(numberGates);
+
+        for (qint16 i = 0; i < numberGates; i++)
+        {
+            g = &(netlist.gates[i]);
+
+            for (qint16 j = 0; j < g->inputs.size(); j++)          // for each input
+            {
+                // check if the each input of the gate is an output of another gate
+                int outin = netlist.find(&(netlist.outputs),g->inputs[j]);
+
+                for (qint16 k = 0; k < numberGates; k++)   // for each output
+                {
+                    g1 = &(netlist.gates[k]);
+                    if (outin!= -1)
+                        if (g1->output == netlist.outputs[outin])
+                            netlist.adjMatrix[i][k] = 1;
+                }
+            }
+
+            // check if gate output is input to other gates
+        }
+
+        /*
+        cout << "file open" << endl;
+
+        cout << "number of gates: " << numberGates << endl;
+
+        for (int i = 0 ; i < numberGates; i++)
+        {
+            cout << netlist.gates[i].name << "  ";
+            for (int j = 0; j < numberGates; j++)
+                cout <<  netlist.adjMatrix[i][j];
+            cout << endl;
+        }
+        */
+
+        /* TOPO SORT */
+        TopoSort top(&netlist);
+        top.KahnSort();
+
+        /*
+        cout << "main" << endl;
+
+        for (int i = 0 ; i < top.sortedGates.size(); i++)
+        {
+            cout << top.sortedGates[i]  << endl;
+        }
+        */
+
+    }
+
 
 }
